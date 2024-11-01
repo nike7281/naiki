561a6ebb46dde6dfd20ccf039934a3acc39f131f
nike7281 <t236013@st.yamagata-u.ac.jp>
2024-11-01 19:08:29 +0900

    Add files via upload

---
+++ tata.C
@@ -0,0 +1,13 @@
+#include <float.h>
+#include <limits.h>
+#include <stdio.h>
+
+int main(void)
+{
+    printf("char型の符号なし最大値：%d\n", UCHAR_MAX);
+    printf("char型の最大値：%d\n", CHAR_MAX);
+    printf("char型の最小値：%d\n", CHAR_MIN);
+    
+
+    return 0;
+}
