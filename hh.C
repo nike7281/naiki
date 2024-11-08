869b2503ed5de83fc9a44d77564b948213ad5d35
nike7281 <t236013@st.yamagata-u.ac.jp>
2024-11-08 19:24:48 +0900

    Add files via upload

---
+++ hh.C
@@ -0,0 +1,26 @@
+int main(void)
+{
+    int i,j,k;
+    
+    printf("how tall are you");
+    scanf("%d" , &i);
+    printf("how much do you weigh")
+    scanf("%d" , &j);
+    
+    k=i+J
+    if (k>=230)
+    ｛
+    　　printf("you have a great body");
+    ｝
+    else　
+    ｛
+    　　printf("you are healthy");
+    ｝
+    else　if
+    ｛
+      　printf("you are thin");
+     ｝
+     
+    
+    return 0;
+    ｝
